d09eff8c93e61db171e0cff0141afe1eb14bee60
gitsamk <skrame18@gmail.com>
2023-09-25 23:12:56 -0700

    slides js

--- js/script.js
+++ js/script.js
@@ -2,37 +2,30 @@ const nxt = document.querySelector('.nxt');
 const prev = document.querySelector('.prev');
 const slide = document.querySelector('.pic');
 const guitars = ['guitar1.jpg', 'guitar2.jpg', 'guitar3.jpg', 'guitar4.jpg'];
-const baseURL = 'images/';
+const baseURL = '../images/';
 let index = 1;
 
-nxt.onclick = function(e) {
+
+nxt.onclick = function (e) {
     e.preventDefault();
-    
-    slide.src = baseURL + guitars[index];
-    index = index + 1;
-    console.log(index);
-
-    if(index >= cars.length) {
+    slide.classList.add('fade-out');
+    setTimeout(() => {
+        slide.src = baseURL + guitars[index];
+        slide.classList.remove('fade-out');
+    }, 1000);
+    index = (index + 1) % guitars.length;
+    if (index >= guitars.length) {
         index = 0;
-      }
-    };
+    }
+};
 
-    prev.onclick = function(b) {
-        b.preventDefault();
-    
-        slide.src = guitars[index] - baseURL;
-        index = index - 1;
-        console.log(index);
-    
-        if(index < guitars.length) {
-            index = 3;
-    
-        }
-    };
-        
-        
-
-
-    
-  
+prev.onclick = function (b) {
+    b.preventDefault();
+    index = (index - 1 + guitars.length) % guitars.length;
+    slide.classList.add('fade-out');
+    setTimeout(() => {
+        slide.src = baseURL + guitars[index];
+        slide.classList.remove('fade-out');
+    }, 1000);
+};
 
